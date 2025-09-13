eefb229340a22606642c5cec9464fa99e1109a2d
Endhrajith T <119486189+ENDHRAJITH@users.noreply.github.com>
2025-09-13 13:27:34 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center  relative">
       <div className="backdrop-blur-md bg-white/10 border border-white/20 p-10 rounded-3xl shadow-lg text-center max-w-xl">
-        <h1 className="text-5xl md:text-6xl font-mono text-white font-bold mb-4">
+        <h1 className="text-5xl md:text-6xl font-mono text-black font-bold mb-4">
           CodeEthnics
         </h1>
         <p className="text-lg md:text-xl font-mono text-black">
